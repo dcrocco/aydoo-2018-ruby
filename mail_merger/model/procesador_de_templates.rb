bb3c1b07c7aaa6d6633eb2dbd0885fab6c774d51
Daniel Cristian Crocco <daniel@blueorange.com>
2018-06-04 22:52:48 -0300

    [ADD] Caso de template con un tag

--- mail_merger/model/procesador_de_templates.rb
+++ mail_merger/model/procesador_de_templates.rb
@@ -1,8 +1,13 @@
 
 class ProcesadorDeTemplates
 
-  def procesar_template(template)
+  def procesar_template(template, datos)
+
+    template.scan(/<.*?>/).each do |value|
+      template = template.gsub(value, datos[value[1, value.length - 2]])
+    end
     template
+
   end
 
 end
--- mail_merger/spec/procesador_de_templates_spec.rb
+++ mail_merger/spec/procesador_de_templates_spec.rb
@@ -5,7 +5,15 @@ describe ProcesadorDeTemplates do
   subject { ProcesadorDeTemplates.new }
 
   it 'El template no recibe modificacion' do
-    subject.procesar_template('template')
+    template_original = 'template'
+    template_procesado = subject.procesar_template(template_original, {})
+    expect(template_original).to eq template_procesado
   end
-  
+
+  it 'El procesador recibe una etiqueta y la reemplaza' do
+    template_original = 'template <etiqueta>'
+    template_procesado = subject.procesar_template(template_original, {"etiqueta" => "Test"})
+    expect(template_procesado).to eq 'template Test'
+  end
+
 end
