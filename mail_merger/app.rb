0500f6739179345b3f9313d847b4c19180264618
Daniel Cristian Crocco <daniel@blueorange.com>
2018-06-05 21:29:40 -0300

    [FIX] Error con envio de mail

--- mail_merger/app.rb
+++ mail_merger/app.rb
@@ -2,10 +2,15 @@ require "sinatra"
 require "json"
 require_relative "model/sender"
 
+configure do
+  set :bind, '0.0.0.0'
+end
+
 post '/' do
     begin
       hash_entrada = JSON.parse(request.body.read)
       sender = Sender.new
+      p hash_entrada
       sender.enviar(hash_entrada)
       status 200
     rescue
--- mail_merger/model/sender.rb
+++ mail_merger/model/sender.rb
@@ -1,4 +1,6 @@
 require_relative "procesador_de_templates"
+require_relative "procesador_de_json"
+require 'mail'
 
 class Sender
 
@@ -8,10 +10,8 @@ class Sender
 
   def enviar(hash_entrada)
     procesador_de_json = ProcesadorDeJson.new(hash_entrada)
-
     (0...hash_entrada["contactos"].length).each {|indice|
-
-      procesador_de_json.procesar(indice)
+      body = procesador_de_json.procesar(indice)
 
       Mail.defaults do
         delivery_method :smtp, address: "localhost", port: 1025
@@ -21,7 +21,7 @@ class Sender
         from hash_entrada["datos"]["remitente"]
         to hash_entrada["contactos"][i]["mail"]
         subject hash_entrada["datos"]["asunto"]
-        body hash_entrada.procesar(indice)
+        body body
       end
     }
   end
