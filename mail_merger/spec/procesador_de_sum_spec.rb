20cf4c567639c12720b8aa69b1be20c3583efff5
Daniel Cristian Crocco <daniel@blueorange.com>
2018-06-26 20:00:57 -0300

    [ADD] El sumador ahora es un poco menos genérico

--- mail_merger/spec/procesador_de_sum_spec.rb
+++ mail_merger/spec/procesador_de_sum_spec.rb
@@ -9,4 +9,16 @@ describe ProcesadorDeSum do
     expect(template_procesado).to eq 'template con suma de montos 25'
   end
 
+  it 'El procesador recibe un tag con una suma de montos con decimales y no lo procesa' do
+    template_original = 'template con suma de montos <sum(5.1, 20.2)>'
+    template_procesado = ProcesadorDeSum.new.procesar(template_original, {})
+    expect(template_procesado).to eq 'template con suma de montos <sum(5.1, 20.2)>'
+  end
+
+  it 'El procesador recibe un tag con sum y palabras dentro y no lo procesa' do
+    template_original = 'template con suma de montos <sum(a, 20)>'
+    template_procesado = ProcesadorDeSum.new.procesar(template_original, {})
+    expect(template_procesado).to eq 'template con suma de montos <sum(a, 20)>'
+  end
+
 end
